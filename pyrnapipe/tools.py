3b87f0530bcab109f1798dc4028c40338dcf19d4
Patrick LOmbard <pdl30@cam.ac.uk>
2015-01-14 10:55:15 +0000

    changed insert method

--- pyrnapipe/tools.py
+++ pyrnapipe/tools.py
@@ -12,6 +12,7 @@ import argparse
 import os, re, sys
 import subprocess
 import shutil
+import math
 
 def head_file(ifile, outfile, num):
 	output = open(outfile, "w")
@@ -32,11 +33,9 @@ def infer_experiment(fastq1, fastq2, bowtie_ref, refbed):
 	align_command = "bowtie2 -x {} -1 tmp/infer_test_1.fq -2 tmp/infer_test_2.fq -S tmp/tmp.sam".format(bowtie_ref)
 	subprocess.call(align_command.split())
 	infercommand = "infer_experiment.py -i tmp/tmp.sam -r {} > tmp/infer_res.txt".format(refbed)
-	insertcommand = "inner_distance.py -i tmp/tmp.sam -o tmp/insert_sizes -r {} > tmp/insert_res.txt".format(refbed)
+	insert = get_insert("tmp/tmp.sam")
 	subprocess.call(infercommand, shell=True)
-	subprocess.call(insertcommand, shell=True)
 	reverse = read_infer()
-	insert = read_insert() #Not tested
 	shutil.rmtree("tmp")
 	return reverse, insert
 
@@ -46,9 +45,9 @@ def read_insert():
 		c = 0
 		for line in f:
 			line = line.rstrip()
-			if c = 1:
+			if c == 1:
 				insert.append(float(line))
-			elif c = 5:
+			elif c == 5:
 				insert.append(float(line))
 			c += 1
 	return insert
@@ -86,3 +85,62 @@ def single_process(fastq, gse, gsm, bowtie_ref, gtf):
 	#subprocess.call(toucsc.split())
 	htseq_count = "pyrna_count.py htseq -i {0}/{1}/accepted_hits.bam -g {2} -o {0}/{1}/{1}.count".format(gse, gsm, gtf)
 	subprocess.call(htseq_count.split())
+
+def getmeanval(dic,maxbound=-1):
+	nsum=0;  n=0;	
+	for (k,v) in dic.items():
+		if maxbound!=-1 and k>maxbound:
+			continue
+		nsum=nsum+k*v;
+		n=n+v;
+	meanv=nsum*1.0/n;
+	nsum=0; n=0;
+	for (k,v) in dic.items():
+		if maxbound!=-1 and k>maxbound:
+			continue;
+		nsum=nsum+(k-meanv)*(k-meanv)*v;
+		n=n+v;
+		varv=math.sqrt(nsum*1.0/(n-1));
+	return [meanv,varv];
+ 
+def get_insert(samfile):
+	plrdlen={};
+	plrdspan={};
+	objmrl=re.compile('([0-9]+)M$');
+	objmtj=re.compile('NH:i:(\d+)');
+	nline=0
+	sam = open(samfile, "r")
+	for lines in sam:
+		field=lines.strip().split();
+		nline=nline+1;
+		if len(field)<12:
+			continue;
+		try:
+			mrl=objmrl.match(field[5]);
+			if mrl==None: # ignore non-perfect reads
+				continue;
+			readlen=int(mrl.group(1));
+			if readlen in plrdlen.keys():
+				plrdlen[readlen]=plrdlen[readlen]+1;
+			else:
+				plrdlen[readlen]=1;
+			if field[6]!='=':
+				continue;
+			dist=int(field[8]);
+			if dist<=0: # ignore neg dist
+				continue;
+			mtj=objmtj.search(lines);
+
+			if dist in plrdspan.keys():
+				plrdspan[dist]=plrdspan[dist]+1;
+			else:
+				plrdspan[dist]=1;
+		except ValueError:
+			continue;
+
+	if len(plrdspan)==0:
+		print('No qualified paired-end reads found. Are they single-end reads?');
+	else:
+		maxv=max(plrdspan,key=plrdspan.get);
+		spanval=getmeanval(plrdspan,maxbound=maxv*3);
+		return spanval
